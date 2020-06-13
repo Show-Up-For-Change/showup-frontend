df09f9e1e09fa4a8ccb046c4367bb50a0ab5658a
Saj King <a.sajking@gmail.com>
2020-06-13 12:42:09 -0400

    Added 2 placeholder actions

--- src/views/containers/ActionsContainer.js
+++ src/views/containers/ActionsContainer.js
@@ -11,10 +11,22 @@ class ActionsContainer extends Component {
 
   render() {
     const {actions} = this.props
+    const exampleAction1 = {
+      subject: 'Podcast',
+      title: 'Throughline: The History of Policing in the US',
+      details: 'A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is. A blurb about what it is.'
+    }
+    const exampleAction2 = {
+      subject: 'Report',
+      title: 'New Era of Public Safety Toolkit (PDF)',
+      details: 'This toolkit is intended to help activists, organizations, and communities identify and act on solutions to change policing for the better in their own communities.'
+    }
     return (
       <div className = "background-white container padding-vertical">
         <div className = "col-4">
           <h2 className ="h2">Learn and Take Action</h2>
+          <Action action = {exampleAction1} />
+          <Action action = {exampleAction2} />
          {actions ? (
             actions.map(action => <Action action = {action} />)
          ) : (
