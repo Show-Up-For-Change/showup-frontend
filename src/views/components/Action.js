9934af57e3f4acec36e4570504f7fc22cd28e934
Jasterix <ljcalixte@gmail.com>
2020-06-13 18:11:23 -0400

    add redux functions

--- src/views/components/Action.js
+++ src/views/components/Action.js
@@ -1,12 +1,16 @@
 import React, {Component} from "react"
 
-export default class Profile extends Component {
+export default class Action extends Component {
 
     render() {
         const {action} = this.props
         return (
             action ? (
-            
+            <div className = 'action-item'>
+                <p className = 'tag'>{action.subject}</p>
+                <h3 className = 'h3 text-grey'>{action.title}</h3>
+                <p className = 'card-text'>{action.details}</p>
+            </div>
             ) : ''
         )
     }
