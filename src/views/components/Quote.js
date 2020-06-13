81bffdb8ef1423ec27f70e58c8a88dfe4b539b9d
Saj King <a.sajking@gmail.com>
2020-06-13 13:23:30 -0400

    Added hardcoded Quote component

---
+++ src/views/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Quote = props => {
+  return (
+    <div className = "background-yellow text-grey padding-vertical">
+      <div className = "container">
+        <h2 className = "h2 h2-alt text-center">
+        In a racist society, it is not enough to be non-racist.<br/>
+			<strong>We must be anti-racist.</strong>
+        </h2>
+        <h3 className = "h3 text-center text-grey">Angela Davis</h3>
+      </div>
+    </div>
+  )
+}
