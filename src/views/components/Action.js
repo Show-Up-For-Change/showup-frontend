5fbbbc4b5f259606475ca00d6983a7741ae2a918
Saj King <a.sajking@gmail.com>
2020-06-13 11:54:34 -0400

    Actions container connected to redux

--- src/views/components/Action.js
+++ src/views/components/Action.js
@@ -1,11 +1,16 @@
 5 lines (95 sloc)  3.41 KB
-  
+
 import React, {Component} from "react"
 import { connect } from "react-redux"
 
 class Action extends Component {
 
+    constructor(props) {
+        super(props)
+    }
+
     render() {
+        const {action} = this.props
         return
     }
 }
--- src/views/containers/ActionsContainer.js
+++ src/views/containers/ActionsContainer.js
@@ -1,27 +1,39 @@
 import React, {Component} from "react"
 import {connect} from 'react-redux';
-
+import fetchActions from '../../store/actions'
+import Action from '../components/Action'
 
 class HashtagsContainer extends Component {
 
   componentDidMount() {
+    this.props.getActions();
+  }
 
   render() {
-
+    const {actions} = this.props
     return (
-
+      <div>
+        {actions ? (
+          actions.map(action => <Action action = {action} />)
+        ) : (
+          ''
+        )}
+      </div>
     )
   }
 }
 
-
   const mapStateToProps = state => {
     return {
-
+      actions: state.actions.actions
     }
   }
 
-  const mapDispatchToProps = {
-
+  const mapDispatchToProps = dispatch => {
+    return {
+      getActions: () => dispatch(fetchActions())
+    }
   }
+
+  export default connect(mapStateToProps, mapDispatchToProps)(HashtagsContainer)
 
