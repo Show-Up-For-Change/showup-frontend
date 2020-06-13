d73e4c3e15e21a26927ad24bef6f7f1d86fb6b8f
Jasterix <ljcalixte@gmail.com>
2020-06-13 00:26:12 -0400

    Updates

---
+++ precache-manifest.412f43fb2d3419782fdb8ddf179dc758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9e868ec8e1b500271dcb8e7fe56d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "854e04f3f8898f8b55b7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "827b9597441468a2acd6",
+    "url": "/static/js/2.cad82b14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cad82b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854e04f3f8898f8b55b7",
+    "url": "/static/js/main.50c7a0e8.chunk.js"
+  },
+  {
+    "revision": "c556d05ecc79d358517e",
+    "url": "/static/js/runtime-main.ebef56e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
