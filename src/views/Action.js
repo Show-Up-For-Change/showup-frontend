4f396552f9cdba403dbb1e6493b30cd250ed353c
Jasterix <ljcalixte@gmail.com>
2020-06-13 00:03:55 -0400

    first build, create basic folder structure

---
+++ src/views/Action.js
@@ -0,0 +1,12 @@
+5 lines (95 sloc)  3.41 KB
+  
+import React, {Component} from "react"
+import { connect } from "react-redux"
+import Moment from 'react-moment'
+
+class Action extends Component {
+
+    render() {
+        return
+    }
+}
