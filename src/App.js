1cd7c786e42b3d913253b0a85857163773963421
Jasterix <ljcalixte@gmail.com>
2020-06-13 20:39:36 -0400

    last stuff done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import ActionsContainer from './views/containers/ActionsContainer'
+import React from "react";
+import logo from "./logo.svg";
+import ActionsContainer from "./views/containers/ActionsContainer";
 import Header from "./views/components/Header";
-import {Quote} from "./views/components/Quote"
-import ThoughtLeadersContainer from './views/containers/ThoughtLeaderContainer'
-import ThoughtLeader from './views/components/ThoughtLeader';
+import { Quote } from "./views/components/Quote";
+import ThoughtLeadersContainer from "./views/containers/ThoughtLeaderContainer";
+import ThoughtLeader from "./views/components/ThoughtLeader";
+import ProfilePage from "./views/containers/ProfilePage";
 
 function App() {
   return (
     <div className="App">
+      <ProfilePage />
       <Header />
-      <ActionsContainer/>
-      <Quote/>
-      <ThoughtLeadersContainer/>
+      <ActionsContainer />
+      <Quote />
+      <ThoughtLeadersContainer />
     </div>
   );
 }
