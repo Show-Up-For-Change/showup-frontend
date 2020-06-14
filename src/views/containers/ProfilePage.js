f492892893d0f5782927c039f18a2563e41c6253
Saj King <a.sajking@gmail.com>
2020-06-14 14:10:03 -0400

    changed profile page logic for testing

--- src/views/containers/ProfilePage.js
+++ src/views/containers/ProfilePage.js
@@ -1,42 +1,42 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {getUserLocalStorage} from '../../localStorage'
-import {Redirect} from 'react-router-dom'
-import NavBar from "../components/NavBar"
-import ProfileHeader from '../components/ProfileHeader'
-import {fetchInfo} from '../../store/user'
-import history from '../../history'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getUserLocalStorage } from "../../localStorage";
+import { Redirect } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import ProfileHeader from "../components/ProfileHeader";
+import { fetchInfo } from "../../store/user";
+import history from "../../history";
 
 class ProfilePage extends Component {
   componentDidMount() {
-    const id = getUserLocalStorage().facebookId
-    this.props.getUserInfo(id)
+    const id = getUserLocalStorage().facebookId;
+    this.props.getUserInfo(id);
   }
 
   goHome() {
-    history.push('/')
+    history.push("/");
   }
 
   render() {
-    const loggedIn = getUserLocalStorage().facebookId
-    return (
-      loggedIn ? (
+    const loggedIn = getUserLocalStorage().facebookId;
+    return true ? (
+      <main id="main-content dashboard">
         <div className="background-black transparecy padding-vertical position-relative hero">
-          <ProfileHeader info = {this.props.info}/>
+          <ProfileHeader info={this.props.info} />
         </div>
-
-      ) : (
-        <div>
-          {this.goHome()}
+        <div class="background-white container padding-vertical">
+          <div class="flex-wrapper position-relative"></div>
         </div>
-      )
-    )
+      </main>
+    ) : (
+      <div>{this.goHome()}</div>
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    info: state.user
+    info: state.user,
   };
 };
 
